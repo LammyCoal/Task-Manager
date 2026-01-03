50fb8c586707c03a102f4ad7ac37e9fc11846fba
Edu Lukmon <edulukmon@gmail.com>
2026-01-03 23:54:17 +0100

    added some feautures to storage.py

--- src/task_manager/storage.py
+++ src/task_manager/storage.py
@@ -6,3 +6,12 @@ from typing import List
 
 DATA_FILE = Path(__file__).parent.parent.parent / "data" / "tasks.json"
 
+def load_tasks() -> List[Task]:
+    if not DATA_FILE.exists():
+        return []
+    with open(DATA_FILE, 'r') as file:
+        json_data = json.load(file)
+
+    tasks = []
+    for task in json_data:
+        due = 
